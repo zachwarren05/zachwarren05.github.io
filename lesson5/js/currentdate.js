07bbec2c43da22b4f144e546fe41827574b9efcb
zachwarren05 <war15016@byui.edu>
2021-02-11 23:11:51 -0700

    initial commit

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -7,6 +7,18 @@ var n = d.getFullYear();
 document.getElementById('date').textContent = day + ", " + d.getDate() + " " + month + " " + n;
 document.getElementById('year').textContent = n;
 
+let message = "";
+
+if (day == 'Friday') {
+    announcement = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    document.querySelector("#saturday").textContent = announcement;
+}
+
+else {
+    let otherDays = document.querySelector("#saturday");
+    otherDays.remove();
+}
+
 function toggleMenu() {
     document
     .getElementsByClassName("navigation")[0]
