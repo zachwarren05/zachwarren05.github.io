1218ce73172feb0fecc3445b46bdf0895477b0f6
zachwarren05 <war15016@byui.edu>
2021-03-05 19:59:41 -0700

    initial commit

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -11,12 +11,16 @@ fetch(requestURL)
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let image = document.createElement('img');
+        let birthinfo = document.createElement('p');
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        birthinfo.textContent = 'Date of Birth: ' + prophets[i].birthdate + "\r\n";
+        birthinfo.textContent += 'Place of Birth: ' + prophets[i].birthplace;
         image.setAttribute('src', prophets[i].imageurl);
         
 
         card.appendChild(h2);
+        card.appendChild(birthinfo);
         card.appendChild(image);
 
         document.querySelector('div.cards').appendChild(card);
