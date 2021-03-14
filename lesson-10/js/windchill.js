8978476f78578f037293756644d074fc2dc5db26
zachwarren32 <zachwarren32@gmail.com>
2021-03-13 19:59:36 -0700

    initial commit

--- lesson-10/js/windchill.js
+++ lesson-10/js/windchill.js
@@ -5,11 +5,15 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('high').textContent = jsObject.main.temp_max;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('speed').textContent = jsObject.wind.speed;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
         const desc = jsObject.weather[0].description;  // note how we reference the weather array
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
         document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
         document.getElementById('icon').setAttribute('alt', desc);
+        document.getElementByClass('description').textContent = desc;
     });
 
 var highTemp = parseFloat(document.getElementById("high").innerText);
