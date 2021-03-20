8561da0c163858697a127af06cf91b5284b0f3e6
zachwarren32 <zachwarren32@gmail.com>
2021-03-20 14:14:58 -0600

    initial commit

--- lesson-11/preston-js/testapi.js
+++
@@ -1,7 +0,0 @@
-const apiForecastURL= "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=adb46ce6ad3541245dc4d3c5158bd147";
-
-fetch(apiForecastURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-    });
